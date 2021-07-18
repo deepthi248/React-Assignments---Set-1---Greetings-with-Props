615a133be9812f29f441ed5cc52b60aca7b742b0
Neghashree <86755686+Neghashree@users.noreply.github.com>
2021-07-18 17:48:45 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,24 @@
 import React from "react";
+
 import '../styles/App.css';
+
 import Welcome from './Welcome';
 
+
+ 
+
 const App = () => {
+
   return (
-    <Welcome name="Ans" />
+
+    <Welcome name="" />
+
   )
+
 }
 
+
+
+ 
 
 export default App;
